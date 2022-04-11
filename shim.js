c3624357ea2f25cf5020579763871d8a6e454278
Jordan Harband <ljharb@gmail.com>
2022-04-11 13:33:31 -0700

    [New] `shim`/`auto`: add `copyWithin` to `Symbol.unscopables`

--- shim.js
+++ shim.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var shimUnscopables = require('es-shim-unscopables');
+
 var getPolyfill = require('./polyfill');
 
 module.exports = function shimArrayCopyWithin() {
 	var polyfill = getPolyfill();
+
 	define(
 		Array.prototype,
 		{ copyWithin: polyfill },
 		{ copyWithin: function () { return Array.prototype.copyWithin !== polyfill; } }
 	);
+
+	shimUnscopables('copyWithin');
+
 	return polyfill;
 };
